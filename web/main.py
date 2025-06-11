27014ec68756d96d7d43f78e5a02580045876723
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2025-06-11 16:58:24 +0000

    Add first drafts of the web application

--- web/main.py
+++ web/main.py
@@ -1,12 +1,78 @@
 # -*- coding: utf-8 -*-
+import time
+import logging
+import os
+
 import gradio as gr
 
+import web
 
-def greet(name, intensity):
-    return 'Hello, ' + name + '!' * int(intensity)
+def _rag_and_chat_stream(user_message, history):
+
+    logging.info('Received user message: %s', user_message)
+    logging.info('Current chat history: %s', history)
+
+    history = history or []
+    
+    chat_history = history.copy()
+    
+    logging.info('Current chat history: %s', history)
+    yield history
+
+    context_docs = web.backend_communication.collect_context_info(
+        user_message=user_message,
+        chat_history=chat_history
+    )
+
+    history[-1][1] = ""
+
+    logging.info('Current chat history: %s', history)
+    yield history
+
+    chat_response = web.backend_communication.stream_chat_response(
+        user_message=user_message,
+        chat_history=chat_history,
+        context_docs=context_docs
+    )
+
+    full_text_response = ""
+    for chunk in chat_response:
+        token = chunk.choices[0].delta.get("content", "")
+        fulfull_text_responsel += token
+        history[-1][1] = full_text_response
+
+        logging.info('Current chat history: %s', history)
+        yield history
+
+
+def _obtain_gui():
+
+    gui = gr.ChatInterface(
+        _rag_and_chat_stream,
+        chatbot=gr.Chatbot(elem_id="agh_chat", height=400, type='tuples', show_copy_button=True),
+        title="AGH Chat",
+        textbox=gr.Textbox(placeholder="Type a message...", label="Your message")
+    )
+
+    return gui
 
 
 if __name__ == '__main__':
-    with gr.Interface(fn=greet,
-                      inputs=['text', 'slider'], outputs=['text']) as demo:
-        demo.title = 'Greeting App'
+
+    persistent_data_path = os.environ['PERSISTENT_DATA_PATH']
+    dev_mode = bool(os.environ['DEV_MODE'])
+
+    logging.basicConfig(
+        level=logging.INFO if dev_mode else logging.DEBUG,
+        format='%(levelname)s - %(asctime)s - %(message)s',
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(os.path.join(
+                persistent_data_path, f'web_{time.time_ns()}.log'), mode='a')
+        ]
+    )
+
+    gui_if = _obtain_gui()
+
+    gui_if.launch(server_name='0.0.0.0',
+               server_port=80)
---
+++ web/src/web/backend_communication.py
@@ -0,0 +1,42 @@
+from typing import List, Dict, Tuple
+import requests
+import os
+import logging
+import httpx
+
+
+def collect_context_info(user_message: str, chat_history: List[Dict[str, str]])-> List[Dict[str, str]]:
+    """Collects context information based on the user's message and chat history."""
+    
+    logging.debug('Collecting context info with user_message: %s and chat_history: %s',
+                  user_message, chat_history)
+
+    url = f"{os.environ['BACKEND_API_URL']}/collect_context_info"
+    payload = {
+        "user_message": user_message,
+        "chat_history": chat_history
+    }
+    
+    response = requests.post(url, json=payload, timeout=5)
+    response.raise_for_status()
+    
+    return response.json().get("context_docs", [])
+    
+def stream_chat_response(user_message: str,
+                        chat_history: List[Tuple[str, str]],
+                        context_docs: List[Dict[str, str]]):
+    """Collects LLM response based on the context and streams it."""
+
+    logging.debug(('Streaming chat response with user_message: %s, ' +
+                   'chat_history: %s, context_docs: %s'),
+                   user_message, chat_history, context_docs)
+    
+    url = f"{os.environ['BACKEND_API_URL']}/stream_chat_response"
+
+    payload = {
+        "user_message": user_message,
+        "chat_history": chat_history,
+        "context_docs": context_docs
+    }
+
+    yield from httpx.stream('POST', url, data=payload, timeout=5)
